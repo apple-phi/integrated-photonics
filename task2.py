f792363fe35ba17a275dea2d0ed03578ff7a924c
apple-phi <ln373@cam.ac.uk>
2025-06-06 12:01:40 +0100

    Basic lumapi

--- task2.py
+++ task2.py
@@ -1,3 +1,5 @@
+import os
+import time
 import sys
 import pathlib
 
@@ -7,7 +9,7 @@ root = pathlib.Path("C:\\Program Files\\Lumerical")
 for dirpath, dirnames, filenames in root.walk():
     if "lumapi.py" in filenames:
         sys.path.append(str(dirpath))
-        print("Found lumapi at:", dirpath / "lumapi.py")
+        print("Found lumapi at: '", dirpath / "lumapi.py'")
         break
 else:
     raise ImportError("lumapi.py not found in the specified directory tree.")
@@ -16,21 +18,223 @@ import lumapi  # type: ignore[import-untyped]
 import numpy as np
 import matplotlib.pyplot as plt
 
-with lumapi.FDTD() as fdtd:
-    lambda_range = np.linspace(300e-9, 1100e-9, 500)
-    c = 2.99792458e8
-    f_range = c / lambda_range
-    au_index = fdtd.getfdtdindex(
-        "Au (Gold) - CRC", f_range, np.min(f_range), np.max(f_range)
-    )  # Use the getfdtdindex command to obtain the correct complex index for gold
+u = 1e-6
 
-    stackRT_result = fdtd.stackrt(
-        np.transpose(au_index), np.array([10e-9]), f_range
-    )  # Use the stackrt command to calculate the transmission and reflection
-# Visualize using matplotlib
-fig, ax = plt.subplots()
-ax.plot(lambda_range * 1e9, stackRT_result["Ts"], label="Transmission")
-ax.set_xlabel("Wavelength [nm]")
-ax.set_ylabel("Transmission")
-ax.legend()
-plt.show()
+# export QT_QPA_PLATFORM=offscreen 
+# os.environ["QT_QPA_PLATFORM"] = "offscreen"
+
+with lumapi.FDTD(hide=True) as fdtd:
+    fdtd.addfdtd(
+        x=0,
+        y=0,
+        z=0,
+        x_span=35 * u,
+        y_span=18 * u,
+        z_span=1 * u,
+        background_material="SiO2 (Glass) - Palik",
+    )
+    # Parallel waveguides
+    wg1 = fdtd.addrect(
+        name="wg_1",
+        material="Si (Silicon) - Palik",
+        x=0,
+        y=0.325 * u,
+        z=0,
+        x_span=10 * u,
+        y_span=0.5 * u,
+        z_span=0.22 * u,
+    )
+    wg2 = fdtd.addrect(
+        name="wg_2",
+        material="Si (Silicon) - Palik",
+        x=0,
+        y=-0.325 * u,
+        z=0,
+        x_span=10 * u,
+        y_span=0.5 * u,
+        z_span=0.22 * u,
+    )
+    # Input / Output connecting s-bends
+    sbend_tr = fdtd.addwaveguide(
+        name="sbend_tr",
+        material="Si (Silicon) - Palik",
+        x=5 * u,
+        y=0.325 * u,
+        z=0,
+        base_width=0.5 * u,
+        base_height=0.22 * u,
+        base_angle=90,
+    )
+    sbend_tr["poles"] = np.array([[0, 0], [5 * u, 0], [5 * u, 5 * u], [10 * u, 5 * u]])
+    sbend_br = fdtd.addwaveguide(
+        name="sbend_br",
+        material="Si (Silicon) - Palik",
+        x=5 * u,
+        y=-0.325 * u,
+        z=0,
+        base_width=0.5 * u,
+        base_height=0.22 * u,
+        base_angle=90,
+    )
+    sbend_br["poles"] = (
+        np.array(
+            [
+                [0, 0],
+                [5, 0],
+                [5, -5],
+                [10, -5],
+            ]
+        )
+        * u
+    )
+    sbend_tl = fdtd.addwaveguide(
+        name="sbend_tl",
+        material="Si (Silicon) - Palik",
+        x=-5 * u,
+        y=0.325 * u,
+        z=0,
+        base_width=0.5 * u,
+        base_height=0.22 * u,
+        base_angle=90,
+    )
+    sbend_tl["poles"] = (
+        np.array(
+            [
+                [0, 0],
+                [-5, 0],
+                [-5, 5],
+                [-10, 5],
+            ]
+        )
+        * u
+    )
+    sbend_bl = fdtd.addwaveguide(
+        name="sbend_bl",
+        material="Si (Silicon) - Palik",
+        x=-5 * u,
+        y=-0.325 * u,
+        z=0,
+        base_width=0.5 * u,
+        base_height=0.22 * u,
+        base_angle=90,
+    )
+    sbend_bl["poles"] = (
+        np.array(
+            [
+                [0, 0],
+                [-5, 0],
+                [-5, -5],
+                [-10, -5],
+            ]
+        )
+        * u
+    )
+    # Input / Output straight waveguides
+    wg_tr = fdtd.addrect(
+        name="wg_tr",
+        material="Si (Silicon) - Palik",
+        x=17.5 * u,
+        y=5.325 * u,
+        z=0,
+        x_span=5 * u,
+        y_span=0.5 * u,
+        z_span=0.22 * u,
+    )
+    wg_br = fdtd.addrect(
+        name="wg_br",
+        material="Si (Silicon) - Palik",
+        x=17.5 * u,
+        y=-5.325 * u,
+        z=0,
+        x_span=5 * u,
+        y_span=0.5 * u,
+        z_span=0.22 * u,
+    )
+    wg_tl = fdtd.addrect(
+        name="wg_tl",
+        material="Si (Silicon) - Palik",
+        x=-17.5 * u,
+        y=5.325 * u,
+        z=0,
+        x_span=5 * u,
+        y_span=0.5 * u,
+        z_span=0.22 * u,
+    )
+    wg_bl = fdtd.addrect(
+        name="wg_bl",
+        material="Si (Silicon) - Palik",
+        x=-17.5 * u,
+        y=-5.325 * u,
+        z=0,
+        x_span=5 * u,
+        y_span=0.5 * u,
+        z_span=0.22 * u,
+    )
+
+    # Mode source
+    source = fdtd.addmode(
+        name="mode_source",
+        x=-17 * u, # need to be just inside FDTD box
+        y=5.325 * u,
+        z=0,
+        injection_axis="x-axis",
+        mode_selection="fundamental TE mode",
+        center_wavelength=1.55*u,
+        wavelength_span=0,
+        # wavelength_start=1.55e-6,
+        # wavelength_stop=1.55e-6,
+    )
+
+    # Mode monitors
+    mon_tr = fdtd.addpower(
+        name="mon_tr",
+        monitor_type="2D X-normal",
+        x=17 * u,  # need to be just inside FDTD box
+        y=5.325 * u,
+        z=0,
+        y_span=4 * u, # No x-span for 2D x-plane
+        z_span=2 * u,
+    )
+    mon_br = fdtd.addpower(
+        name="mon_br",
+        monitor_type="2D X-normal",
+        x=17 * u,  # need to be just inside FDTD box
+        y=-5.325 * u,
+        z=0,
+        y_span=4 * u,
+        z_span=2 * u,
+    )
+    mon_znorm = fdtd.addpower(
+        name="mon_zplane",
+        monitor_type="2D Z-normal",
+        x=0,
+        y=0,
+        z=0,
+        x_span=34 * u,
+        y_span=14 * u,
+    )
+
+    # Mode expansion monitors
+    me_tr = fdtd.addmodeexpansion(
+        name="me_tr",
+        mode_selection="fundamental TE mode",
+        monitor_type="2D X-normal",
+        x=17 * u,  # need to be just inside FDTD box
+        y=-5.325 * u,
+        z=0,
+        y_span=4 * u,
+        z_span=2 * u,
+    )
+    fdtd.setexpansion("input", "mon_tr")
+    me_br = fdtd.addmodeexpansion(
+        name="me_br",
+        mode_selection="fundamental TE mode",
+        monitor_type="2D X-normal",
+        x=17 * u,  # need to be just inside FDTD box
+        y=-5.325 * u,
+        z=0,
+        y_span=4 * u,
+        z_span=2 * u,
+    )
+    fdtd.setexpansion("input", "mon_br")
+    fdtd.run()
--- types/lumapi.pyi
+++ types/lumapi.pyi
@@ -236,3 +236,121 @@ class MODE(Lumerical):
         remoteArgs={},
         **kwargs,
     ) -> None: ...
+
+# class Lumerical(object):
+#     def __init__(self, product, filename, key, hide, serverArgs, remoteArgs, **kwargs):
+#         """Keyword Arguments:
+#                 script: A single string containing a script filename, or a collection of strings
+#                         that are filenames. Preffered types are list and tuple, dicts are not
+#                         supported. These scripts will run after the project specified by the
+#                         project keyword is opened. If no project is specified, they will run
+#                         in a new blank project.
+
+#                 project: A single string containing a project filename. This project will be
+#                          opened before any scripts specified by the script keyword are run.
+#         """
+#          # this is to keep backward compatibility with applications that need a CAD running until the 
+#          # Python interpreter shuts down
+#         self.keepCADOpened = self.__extractKeepCADOpenedArgument__(serverArgs)
+#         iapi = initLib(remoteArgs)
+#         handle = self.__open__(iapi, product, key, hide, serverArgs, remoteArgs)
+#         self.handle = LumApiSession(iapi, handle)
+
+#         self.syncUserFunctionsFlag = False
+#         self.userFunctions = set() # variable to keep track of all added user methods
+
+#         # get a list of commands from script interpreter and register them
+#         # an error here is a constructor failure to populate class methods
+#         try:
+#             self.eval('api29538 = getcommands;')
+#             commands = self.getv('api29538').split("\n")
+#             commands = [x for x in commands if len(x) > 0 and x[0].isalpha()]
+#             self.eval('clear(api29538);')
+#         except:
+#             close(self.handle)
+#             raise
+
+#         try:
+#             with biopen(INTEROPLIBDIR + '/docs.json') as docFile:
+#                 docs = json.load(docFile)
+#         except:
+#             docs = {}
+
+#         # add methods to class corresponding to Lumerical script
+#         # use lambdas to create closures on the name argument.
+#         keywordsLumerical = ['for', 'if', 'else', 'exit', 'break', 'del', 'eval', 'try', 'catch', 'assert', 'end',
+#                              'true', 'false', 'isnull']
+#         deprecatedScriptCommands = ['addbc', 'addcontact', 'addeigenmode', 'addpropagator', 'deleteallbc', 'deletebc',
+#                                     'getasapdata', 'getbc', 'getcompositionfraction', 'getcontact', 'getglobal',
+#                                     'importdoping', 'lum2mat', 'monitors', 'new2d', 'new3d', 'newmode', 'removepropertydependency',
+#                                     'setbc', 'setcompositionfraction', 'setcontact', 'setglobal', 'setsolver', 'setparallel',
+#                                     'showdata', 'skewness', 'sources', 'structures']
+#         functionsToExclude = keywordsLumerical + deprecatedScriptCommands
+
+#         addScriptCommands =    ['add2drect', 'add2dpoly', 'addabsorbing', 'addanalysisgroup', 'addanalysisprop',
+#                                 'addanalysisresult', 'addbandstructuremonitor', 'addbulkgen', 'addchargemesh',
+#                                 'addchargemonitor', 'addchargesolver', 'addcircle', 'addconvectionbc',
+#                                 'addctmaterialproperty', 'addcustom', 'adddeltachargesource', 'addelectricalcontact',
+#                                 'addelement', 'addemabsorptionmonitor', 'addemfieldmonitor', 'addemfieldtimemonitor',
+#                                 'addemmaterialproperty', 'adddevice', 'adddgtdmesh', 'adddgtdsolver', 'adddiffusion',
+#                                 'addimplant', 'adddipole', 'adddope', 'addeffectiveindex', 'addefieldmonitor',
+#                                 'addelectricalcontact', 'addelement', 'addeme', 'addemeindex', 'addemeport',
+#                                 'addemeprofile', 'addfde', 'addfdtd', 'addfeemsolver', 'addfeemmesh', 'addgaussian',
+#                                 'addgridattribute', 'addgroup', 'addheatfluxbc', 'addheatfluxmonitor', 'addheatmesh',
+#                                 'addheatsolver', 'addhtmaterialproperty', 'addimport', 'addimportdope',
+#                                 'addimportedsource', 'addimportgen', 'addimportheat', 'addimporttemperature',
+#                                 'addindex', 'addjfluxmonitor', 'addlayer', 'addlayerbuilder',
+#                                 'addimportnk', 'addmesh', 'addmode', 'addmodeexpansion', 'addmodelmaterial',
+#                                 'addmodesource', 'addmovie', 'addobject', 'addparameter', 'addpath', 'addpec',
+#                                 'addperiodic', 'addplane', 'addplanarsolid', 'addpmc', 'addpml', 'addpoly', 'addpower',
+#                                 'addprofile', 'addproperty', 'addpyramid', 'addradiationbc', 'addrect',
+#                                 'addring', 'addsimulationregion', 'addsphere', 'addstructuregroup', 'addsurface',
+#                                 'addsurfacerecombinationbc', 'addtemperaturebc', 'addtemperaturemonitor', 'addtfsf',
+#                                 'addthermalinsulatingbc', 'addthermalpowerbc', 'addtime', 'addtriangle',
+#                                 'adduniformheat', 'adduserprop', 'addvarfdtd', 'addvoltagebc', 'addwaveguide']
+
+#         for name in [n for n in commands if n not in functionsToExclude]:
+#             if name in addScriptCommands:
+#                 method = (lambda x: lambda self, *args, **kwargs:
+#                 appCallWithConstructor(self, x, args, **kwargs))(name)
+#             else:
+#                 method = (lambda x: lambda self, *args: appCall(self, x, args))(name)
+#             method.__name__ = str(name)
+#             try:
+#                 method.__doc__ = docs[name]['text'] + "\n" + docs[name]['link']
+#             except:
+#                 pass
+#             setattr(Lumerical, name, method)
+
+#         # change the working directory to match Python program
+#         # load or run any file provided as argument
+#         # an error here is a constructor failure due to invalid user argument
+#         try:
+#             if REMOTE_MODULE_ON is False: # we are not on remote mode
+#                 self.cd(os.getcwd())
+#             if filename is not None:
+#                 if filename.endswith('.lsf'):
+#                     self.feval(filename)
+#                 elif filename.endswith('.lsfx'):
+#                     self.eval(filename[:-5] + ';')
+#                 else:
+#                     self.load(filename)
+
+#             if kwargs is not None:
+#                 if 'project' in kwargs:
+#                     self.load(kwargs['project'])
+#                 if 'script' in kwargs:
+#                     if type(kwargs['script']) is not str:
+#                         for script in kwargs['script']:
+#                             if script.endswith('.lsfx'):
+#                                 self.eval(script[:-5] + ';')
+#                             else:
+#                                 self.feval(script)
+#                     else:
+#                         if kwargs['script'].endswith('.lsfx'):
+#                             self.eval(kwargs['script'][:-5] + ';')
+#                         else:
+#                             self.feval(kwargs['script'])
+#         except:
+#             close(self.handle)
+#             raise
